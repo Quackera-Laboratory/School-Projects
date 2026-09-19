b0dd7a2f9043110bd416c23585004d16eca28468
Quackera-Laboratory <blowe393@hcc.edu>
2026-09-19 02:13:34 -0400

    print dialog for mimic banking frontend
    Literally just a print statement

---
+++ Basic Bank Frontend.py
@@ -0,0 +1,7 @@
+checl_acount = 1234.10
+sav_acount = 4321.01
+#Withdraw
+#Deposit
+#Transfer
+
+print("Hello! What would you like to do today? \nChecking? \n   Saving? \n     Withdraw? \n       deposit? \n \t Transfer? ")
