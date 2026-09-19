fd3d26fbb1130e2c3a938322baa055558c18de23
Quackera-Laboratory <blowe393@hcc.edu>
2026-09-19 02:23:08 -0400

    Added integers and variables

--- Basic Bank Frontend.py
+++ Basic Bank Frontend.py
@@ -4,4 +4,6 @@ sav_acount = 4321.01
 #Deposit
 #Transfer
 
-print("Hello! What would you like to do today? \nChecking? \n   Saving? \n     Withdraw? \n       deposit? \n \t Transfer? ")
+def print_page1():
+    print("Hello! What would you like to do today? \nChecking? \n   Saving? \n     Withdraw? \n       deposit? \n \t Transfer? ")
+print_page1()
